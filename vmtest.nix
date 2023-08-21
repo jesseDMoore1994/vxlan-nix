1163036c5d9ae7dd980f8c5e09ae7beecb2a450f
Jesse Moore <jesse@jessemoore.dev>
2023-08-20 23:43:11 -0500

    finally get traffic flowing over vxlan

--- vmtest.nix
+++ vmtest.nix
@@ -3,40 +3,6 @@
 let
   wg-snakeoil-keys = import ./snakeoil-keys.nix;
   peer = (import ./make-peer.nix) { inherit lib; };
-  peer0-vxlan-orchestration = {
-    path = [ pkgs.git pkgs.iproute2 pkgs.nettools pkgs.nix ];
-    script = with pkgs; ''
-      #!/usr/bin/env nix-shell
-      #!nix-shell -i bash
-
-      ${iproute2}/bin/ip address add dev br0 10.23.43.1/24
-      ${iproute2}/bin/ip link add peer1    type vxlan remote 10.23.42.2 id 1 dstport 4789
-
-    '';
-
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
-    };
-    wantedBy = [ "multi-user.target" ];
-    after = ["wireguard-wg0.service"];
-  };
-  peer1-vxlan-orchestration = {
-    path = [ pkgs.git pkgs.iproute2 pkgs.nettools pkgs.nix ];
-    script = with pkgs; ''
-      #!/usr/bin/env nix-shell
-      #!nix-shell -i bash
-
-      ${iproute2}/bin/ip address add dev br0 10.23.43.2/24
-      ${iproute2}/bin/ip link add peer0    type vxlan remote 10.23.42.1 id 1 dstport 4789
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
-    };
-    wantedBy = [ "multi-user.target" ];
-    after = ["wireguard-wg0.target"];
-  };
 in
   nixosTest {
     name = "vxlan-nix";
@@ -45,7 +11,7 @@ in
         ip4 = "192.168.0.1";
         ip6 = "fd00::1";
         extraConfig = {
-          networking.firewall.allowedUDPPorts = [ 23542 ];
+          networking.firewall.allowedUDPPorts = [ 8472 23542 ];
           networking.wireguard.interfaces.wg0 = {
             ips = [ "10.23.42.1/32" "fc00::1/128" ];
             listenPort = 23542;
@@ -59,17 +25,18 @@ in
             };
           };
           systemd.services.peer0-vxlan-orchestration = {
-            path = [ pkgs.git pkgs.iproute2 pkgs.nettools pkgs.nix ];
+            path = [ pkgs.bridge-utils pkgs.git pkgs.iproute2 pkgs.nettools pkgs.nix ];
             enable = true;
             script = with pkgs; ''
               #!/usr/bin/env nix-shell
               #!nix-shell -i bash
-
-              ${iproute2}/bin/ip link add name br0 type bridge stp_state 1
-              ${iproute2}/bin/ip address add dev br0 10.23.43.1/24
-              ${iproute2}/bin/ip link add peer1    type vxlan remote 10.23.42.2 id 1 dstport 4789
-              ${iproute2}/bin/ip link set up peer1
-              ${iproute2}/bin/ip link set peer1 master br0
+              ${iproute2}/bin/ip link add vxlan100 type vxlan id 100 local 10.23.42.1 remote 10.23.42.2
+              ${iproute2}/bin/ip link set vxlan100 up
+              ${bridge-utils}/bin/brctl addbr br100
+              ${bridge-utils}/bin/brctl addif br100 vxlan100
+              ${bridge-utils}/bin/brctl stp br100 off
+              ${iproute2}/bin/ip link set br100 up
+              ${iproute2}/bin/ip addr add 192.168.3.1/24 dev br100
 
               echo "" > /peer0
             '';
@@ -88,6 +55,7 @@ in
         ip4 = "192.168.0.2";
         ip6 = "fd00::2";
         extraConfig = {
+          networking.firewall.allowedUDPPorts = [ 8472 23542 ];
           networking.wireguard.interfaces.wg0 = {
             ips = [ "10.23.42.2/32" "fc00::2/128" ];
             listenPort = 23542;
@@ -109,17 +77,19 @@ in
             '';
           };
           systemd.services.peer1-vxlan-orchestration = {
-            path = [ pkgs.git pkgs.iproute2 pkgs.nettools pkgs.nix ];
+            path = [ pkgs.bridge-utils pkgs.git pkgs.iproute2 pkgs.nettools pkgs.nix ];
             enable = true;
             script = with pkgs; ''
               #!/usr/bin/env nix-shell
               #!nix-shell -i bash
 
-              ${iproute2}/bin/ip link add name br0 type bridge stp_state 1
-              ${iproute2}/bin/ip address add dev br0 10.23.43.2/24
-              ${iproute2}/bin/ip link add peer0    type vxlan remote 10.23.42.1 id 1 dstport 4789
-              ${iproute2}/bin/ip link set up peer0
-              ${iproute2}/bin/ip link set peer0 master br0
+              ${iproute}/bin/ip link add vxlan100 type vxlan id 100 local 10.23.42.2 remote 10.23.42.1
+              ${iproute}/bin/ip link set vxlan100 up
+              ${bridge-utils}/bin/brctl addbr br100
+              ${bridge-utils}/bin/brctl addif br100 vxlan100
+              ${bridge-utils}/bin/brctl stp br100 off
+              ${iproute}/bin/ip link set br100 up
+              ${iproute}/bin/ip addr add 192.168.3.2/24 dev br100
 
               echo "" > /peer1
             '';
@@ -144,6 +114,6 @@ in
  
       peer1.succeed("ping -c5 fc00::1")
       peer1.succeed("ping -c5 10.23.42.1")
-      peer1.succeed("ping -c5 10.23.43.1")
+      peer1.succeed("ping -c5 192.168.3.1")
     '';
   }
